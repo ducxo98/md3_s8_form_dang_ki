6b0725ca0a7f921d0d22b343fa898df7c914e0dd
duc <ducxo98@gmail.com>
2019-09-04 14:27:30 +0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RegisterFinalComponent } from './register-final/register-final.component';
+import { TodoComponent } from './todo/todo.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    RegisterFinalComponent,
+    TodoComponent,
+    TemplateDrivenFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
